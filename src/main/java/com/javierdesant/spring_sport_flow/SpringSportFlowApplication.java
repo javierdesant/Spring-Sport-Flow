5b0a5e3e4e10c4e4b6e489f6da7bd21f6f12aac2
JavierDeSant <j.desantiagosoto@gmail.com>
2024-12-16 18:19:26 +0100

    first commit

---
+++ src/main/java/com/javierdesant/spring_sport_flow/SpringSportFlowApplication.java
@@ -0,0 +1,13 @@
+package com.javierdesant.spring_sport_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSportFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSportFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javierdesant/spring_sport_flow/SpringSportFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javierdesant.spring_sport_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSportFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
