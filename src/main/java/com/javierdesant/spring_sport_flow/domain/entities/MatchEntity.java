0aabe9d361ec074a5ab65f78993d119589368bdc
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-19 16:24:23 +0100

    feat(MatchEntity): add JPA mapping and Lombok annotations
    Introduced JPA annotations for entity mapping and Lombok for cleaner code. Added new fields for ID, tournament association, and matchup type to enhance entity capabilities.

--- src/main/java/com/javierdesant/spring_sport_flow/domain/entities/MatchEntity.java
+++ src/main/java/com/javierdesant/spring_sport_flow/domain/entities/MatchEntity.java
@@ -1,7 +1,27 @@
 package com.javierdesant.spring_sport_flow.domain.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
+@Entity(name = "matchups")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class MatchEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "tournament_id", nullable = false)
+    private TournamentEntity tournament;
+
+    @Column(length = 20, nullable = false)
+    private String matchupType;
+
 }
