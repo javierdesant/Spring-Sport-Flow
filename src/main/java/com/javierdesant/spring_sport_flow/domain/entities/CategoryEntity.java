78253bcbf854be960bf6a8eac454624b72a4c181
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-20 20:41:52 +0100

    fix(CategoryEntity): add missing relationship for statistics
    Added a new OneToMany relationship with StatisticEntity, enabling cascade operations and orphan removal. This improves the entity's management of related statistics, ensuring consistency and automatic cleanup when necessary.

--- src/main/java/com/javierdesant/spring_sport_flow/domain/entities/CategoryEntity.java
+++ src/main/java/com/javierdesant/spring_sport_flow/domain/entities/CategoryEntity.java
@@ -1,5 +1,6 @@
 package com.javierdesant.spring_sport_flow.domain.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.OneToMany;
@@ -20,4 +21,7 @@ public class CategoryEntity extends BaseCodeEntity {
 
     @OneToMany(mappedBy = "category")
     private Set<TournamentEntity> tournaments;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<StatisticEntity> statistics;
 }
