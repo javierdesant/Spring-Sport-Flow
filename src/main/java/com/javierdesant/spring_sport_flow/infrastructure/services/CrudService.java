798484cb335d6979d8ed9b80cddce46c423075a9
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-20 15:02:33 +0100

    feat(services): add generic CrudService interface
    Introduce a generic interface to standardize CRUD operations. This provides a reusable contract for service classes, improving maintainability and reducing code duplication.

---
+++ src/main/java/com/javierdesant/spring_sport_flow/infrastructure/services/CrudService.java
@@ -0,0 +1,13 @@
+package com.javierdesant.spring_sport_flow.infrastructure.services;
+
+public interface CrudService<RQ, RS, ID> {
+
+    RS create(RQ request);
+
+    RS read(ID id);
+
+    RS update(RQ request, ID id);
+
+    void delete(ID id);
+
+}
