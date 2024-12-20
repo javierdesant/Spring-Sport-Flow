cdd20f3423eee05bdb6fb6589e60ec78b99a8c56
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-20 20:42:58 +0100

    feat(dto): add response DTOs for categories, sports, and leagues
    Introduced `BaseCodeResponse` abstract class and its extensions: `CategoryResponse`, `SportResponse`, and `LeagueResponse`. These DTOs standardize response handling for categories, sports, and leagues in the API.

---
+++ src/main/java/com/javierdesant/spring_sport_flow/api/dto/responses/BaseCodeResponse.java
@@ -0,0 +1,18 @@
+package com.javierdesant.spring_sport_flow.api.dto.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@SuperBuilder
+public abstract class BaseCodeResponse implements Serializable {
+    private String code;
+    private String name;
+}
---
+++ src/main/java/com/javierdesant/spring_sport_flow/api/dto/responses/LeagueResponse.java
@@ -0,0 +1,19 @@
+package com.javierdesant.spring_sport_flow.api.dto.responses;
+
+import com.javierdesant.spring_sport_flow.domain.entities.TournamentEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Data
+@SuperBuilder
+public class LeagueResponse extends BaseCodeResponse {
+    private Set<TournamentEntity> tournaments;
+}
