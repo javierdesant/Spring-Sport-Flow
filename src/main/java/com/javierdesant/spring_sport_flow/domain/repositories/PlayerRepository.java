a6a4b714326ef0294693d1a8a815f2cbe33985f6
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-20 18:37:53 +0100

    feat(PlayerService): add service implementation for player CRUD
    Added `PlayerService` with basic CRUD method stubs and an initial implementation for the `create` method. Also enhanced `PlayerRepository` with a `findByDni` method to support player retrieval by DNI.

--- src/main/java/com/javierdesant/spring_sport_flow/domain/repositories/PlayerRepository.java
+++ src/main/java/com/javierdesant/spring_sport_flow/domain/repositories/PlayerRepository.java
@@ -3,6 +3,9 @@ package com.javierdesant.spring_sport_flow.domain.repositories;
 import com.javierdesant.spring_sport_flow.domain.entities.PlayerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
+    Optional<PlayerEntity> findByDni(String dni);
 
 }
