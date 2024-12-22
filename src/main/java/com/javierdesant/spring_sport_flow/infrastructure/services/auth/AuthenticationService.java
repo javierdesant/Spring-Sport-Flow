822edd736d18c01875149d97910cc917a9089f55
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-22 21:51:56 +0100

    feat(auth): add login functionality to AuthenticationService
    Introduce a new login method handling user authentication. This implementation uses AuthenticationManager to verify credentials and generates a JWT for authenticated players.

--- src/main/java/com/javierdesant/spring_sport_flow/infrastructure/services/auth/AuthenticationService.java
+++ src/main/java/com/javierdesant/spring_sport_flow/infrastructure/services/auth/AuthenticationService.java
@@ -1,10 +1,16 @@
 package com.javierdesant.spring_sport_flow.infrastructure.services.auth;
 
+import com.javierdesant.spring_sport_flow.api.dto.auth.AuthenticationRequest;
+import com.javierdesant.spring_sport_flow.api.dto.auth.AuthenticationResponse;
 import com.javierdesant.spring_sport_flow.api.dto.mappers.PlayerMapper;
 import com.javierdesant.spring_sport_flow.api.dto.requests.PlayerRequest;
 import com.javierdesant.spring_sport_flow.api.dto.responses.PlayerResponse;
 import com.javierdesant.spring_sport_flow.domain.entities.PlayerEntity;
 import com.javierdesant.spring_sport_flow.infrastructure.services.PlayerService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -16,11 +22,13 @@ public class AuthenticationService {
     private final PlayerService playerService;
     private final JwtService jwtService;
     private final PlayerMapper playerMapper;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthenticationService(PlayerService playerService, JwtService jwtService, PlayerMapper playerMapper) {
+    public AuthenticationService(PlayerService playerService, JwtService jwtService, PlayerMapper playerMapper, AuthenticationManager authenticationManager) {
         this.playerService = playerService;
         this.jwtService = jwtService;
         this.playerMapper = playerMapper;
+        this.authenticationManager = authenticationManager;
     }
 
     public PlayerResponse register(PlayerRequest request) {
@@ -39,5 +47,21 @@ public class AuthenticationService {
         extraClaims.put("authorities", player.getAuthorities());
 
         return extraClaims;
+    }
+
+    public AuthenticationResponse login(AuthenticationRequest request) {
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                request.getUsername(), request.getPassword()
+        );
+
+        authenticationManager.authenticate(authentication);
+
+        // FIXME!: the login only contemplates players
+        PlayerEntity player = playerService.findOneByUsername(request.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("Player not found"));
+        String jwt = jwtService.generateToken(player, generateExtraClaims(player));
+
+        return new AuthenticationResponse(jwt);
     }
 }
