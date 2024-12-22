e47cfa28ce8c378859dd1e32071d9d0cf548526c
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-22 02:38:43 +0100

    feat(Tournament): add validation to tournament creation
    Added validation annotations to `TournamentRequest` fields, ensuring proper input (e.g., non-blank, valid dates). Enhanced the `post` method in `TournamentController` by integrating `@Valid` for request validation.

--- src/main/java/com/javierdesant/spring_sport_flow/api/dto/requests/TournamentRequest.java
+++ src/main/java/com/javierdesant/spring_sport_flow/api/dto/requests/TournamentRequest.java
@@ -1,5 +1,9 @@
 package com.javierdesant.spring_sport_flow.api.dto.requests;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +17,25 @@ import java.time.LocalDate;
 @Data
 @Builder
 public class TournamentRequest implements Serializable {
+
+    @NotBlank
     private String tournamentName;
+
+    @NotNull
+    @FutureOrPresent
     private LocalDate startDate;
+
+    @NotNull
+    @Future
     private LocalDate endDate;
+
+    @NotBlank
     private String leagueCode;
+
+    @NotBlank
     private String sportCode;
+
+    @NotBlank
     private String categoryCode;
+
 }
--- src/main/java/com/javierdesant/spring_sport_flow/infrastructure/controllers/TournamentController.java
+++ src/main/java/com/javierdesant/spring_sport_flow/infrastructure/controllers/TournamentController.java
@@ -3,6 +3,7 @@ package com.javierdesant.spring_sport_flow.infrastructure.controllers;
 import com.javierdesant.spring_sport_flow.api.dto.requests.TournamentRequest;
 import com.javierdesant.spring_sport_flow.api.dto.responses.TournamentResponse;
 import com.javierdesant.spring_sport_flow.infrastructure.services.contracts.ITournamentService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,7 +18,7 @@ public class TournamentController {
     private final ITournamentService tournamentService;
 
     @PostMapping
-    public ResponseEntity<TournamentResponse> post(@RequestBody TournamentRequest request) {
+    public ResponseEntity<TournamentResponse> post(@Valid @RequestBody TournamentRequest request) {
         return ResponseEntity.ok(tournamentService.create(request));
     }
 
