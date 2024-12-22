2e6fabee65a408d9d0a593075b56cdcb9d8809f3
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-22 19:24:33 +0100

    fix(TournamentMapper): streamline mapping logic
    Replaced manual mapper method and included relevant sub-mappers for League, Sport, and Category. Simplified and standardized mapping configuration by introducing a Spring component model and removing unused methods.

--- src/main/java/com/javierdesant/spring_sport_flow/api/dto/mappers/TournamentMapper.java
+++ src/main/java/com/javierdesant/spring_sport_flow/api/dto/mappers/TournamentMapper.java
@@ -1,30 +1,18 @@
 package com.javierdesant.spring_sport_flow.api.dto.mappers;
 
-import com.javierdesant.spring_sport_flow.api.dto.responses.CategoryResponse;
-import com.javierdesant.spring_sport_flow.api.dto.responses.LeagueResponse;
-import com.javierdesant.spring_sport_flow.api.dto.responses.SportResponse;
 import com.javierdesant.spring_sport_flow.api.dto.responses.TournamentResponse;
-import com.javierdesant.spring_sport_flow.domain.entities.CategoryEntity;
-import com.javierdesant.spring_sport_flow.domain.entities.LeagueEntity;
-import com.javierdesant.spring_sport_flow.domain.entities.SportEntity;
 import com.javierdesant.spring_sport_flow.domain.entities.TournamentEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring", uses = {LeagueMapper.class, SportMapper.class, CategoryMapper.class})
 public interface TournamentMapper {
-
-    TournamentMapper INSTANCE = Mappers.getMapper(TournamentMapper.class);
-
-    @Mapping(target = "category", source = "category")
-    @Mapping(target = "sport", source = "sport")
-    @Mapping(target = "league", source = "league")
+    @Mapping(target = "tournamentId")
+    @Mapping(target = "tournamentName")
+    @Mapping(target = "startDate")
+    @Mapping(target = "endDate")
+    @Mapping(target = "league")
+    @Mapping(target = "sport")
+    @Mapping(target = "category")
     TournamentResponse toResponse(TournamentEntity entity);
-
-    CategoryResponse toCategoryResponse(CategoryEntity category);
-
-    SportResponse toSportResponse(SportEntity sport);
-
-    LeagueResponse toLeagueResponse(LeagueEntity league);
 }
