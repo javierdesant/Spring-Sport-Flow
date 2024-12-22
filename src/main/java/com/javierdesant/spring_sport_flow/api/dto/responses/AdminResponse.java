9c3aa413629568e4e8be96f8a987dd4af603e782
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-12-22 19:24:49 +0100

    fix(AdminResponse): remove repeated annotations
    Removed @NoArgsConstructor and @AllArgsConstructor annotations as they are not needed. This streamlines the code and ensures consistency with other DTO classes.

--- src/main/java/com/javierdesant/spring_sport_flow/api/dto/responses/AdminResponse.java
+++ src/main/java/com/javierdesant/spring_sport_flow/api/dto/responses/AdminResponse.java
@@ -1,13 +1,9 @@
 package com.javierdesant.spring_sport_flow.api.dto.responses;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
