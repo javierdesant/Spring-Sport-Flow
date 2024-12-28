54c6ad9c829f3d52ef8a5ce6b01e2776a9f886b4
JavierDeSant <j.desantiagosoto@gmail.com>
2024-12-28 17:45:09 +0100

    feat(PlayerController): add profile endpoint for player retrieval
    Introduces a new GET endpoint (/players/me) in PlayerController to fetch the logged-in player's profile. It includes logic to handle user verification and maps PlayerEntity to PlayerResponse using PlayerMapper.
    
    Signed-off-by: JavierDeSant <j.desantiagosoto@gmail.com>

--- src/main/java/com/javierdesant/spring_sport_flow/infrastructure/controllers/PlayerController.java
+++ src/main/java/com/javierdesant/spring_sport_flow/infrastructure/controllers/PlayerController.java
@@ -1,16 +1,16 @@
 package com.javierdesant.spring_sport_flow.infrastructure.controllers;
 
+import com.javierdesant.spring_sport_flow.api.dto.mappers.PlayerMapper;
 import com.javierdesant.spring_sport_flow.api.dto.requests.PlayerRequest;
 import com.javierdesant.spring_sport_flow.api.dto.responses.PlayerResponse;
+import com.javierdesant.spring_sport_flow.domain.entities.PlayerEntity;
+import com.javierdesant.spring_sport_flow.domain.entities.UserEntity;
 import com.javierdesant.spring_sport_flow.infrastructure.services.AuthenticationService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/players")
@@ -18,10 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class PlayerController {
 
     private final AuthenticationService authenticationService;
+    private final PlayerMapper playerMapper;
 
     @PostMapping
     public ResponseEntity<PlayerResponse> register(@RequestBody @Valid PlayerRequest request) {
         PlayerResponse registeredPlayer = authenticationService.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(registeredPlayer);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<PlayerResponse> profile() {
+        UserEntity user = authenticationService.findLoggedInUser();
+        if (user instanceof PlayerEntity player) {
+            PlayerResponse response = playerMapper.toPlayerResponse(player);
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
