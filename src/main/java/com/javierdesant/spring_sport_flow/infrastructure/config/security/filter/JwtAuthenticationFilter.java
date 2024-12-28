3c355cab258568a694af5c10ae46270d3d2d9c8a
JavierDeSant <j.desantiagosoto@gmail.com>
2024-12-28 17:18:17 +0100

    refactor(JwtAuthenticationFilter): add Lombok @AllArgsConstructor
    Replaced the manual constructor with Lombok's @AllArgsConstructor to streamline code and reduce boilerplate. This change improves maintainability by leveraging Lombok annotations for constructor generation.
    
    Signed-off-by: JavierDeSant <j.desantiagosoto@gmail.com>

--- src/main/java/com/javierdesant/spring_sport_flow/infrastructure/config/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/javierdesant/spring_sport_flow/infrastructure/config/security/filter/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,15 +18,11 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@AllArgsConstructor
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenService jwtTokenService;
     private final PlayerService playerService;
-
-    public JwtAuthenticationFilter(JwtTokenService jwtTokenService, PlayerService playerService) {
-        this.jwtTokenService = jwtTokenService;
-        this.playerService = playerService;
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
