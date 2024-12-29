5ba204a7912956a42954d6872fe0a020a151d4ff
JavierDeSant <j.desantiagosoto@gmail.com>
2024-12-29 13:07:59 +0100

    refactor: remove redundant mappers and update TournamentMapper
    Removed unused mappers (CategoryMapper, LeagueMapper, and SportMapper) to reduce code redundancy. Updated TournamentMapper to exclude these dependencies since they are no longer needed. This simplifies the codebase and improves maintainability.
    
    Signed-off-by: JavierDeSant <j.desantiagosoto@gmail.com>

--- src/main/java/com/javierdesant/spring_sport_flow/api/dto/mappers/TournamentMapper.java
+++ src/main/java/com/javierdesant/spring_sport_flow/api/dto/mappers/TournamentMapper.java
@@ -4,7 +4,7 @@ import com.javierdesant.spring_sport_flow.api.dto.responses.TournamentResponse;
 import com.javierdesant.spring_sport_flow.domain.entities.TournamentEntity;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring", uses = {LeagueMapper.class, SportMapper.class, CategoryMapper.class})
+@Mapper(componentModel = "spring")
 public interface TournamentMapper {
     TournamentResponse toTournamentResponse(TournamentEntity entity);
 }
