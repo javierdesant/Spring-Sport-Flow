a382ecb6c6af365ba839a4a6f8f818e7c195f3d4
JavierDeSant <j.desantiagosoto@gmail.com>
2025-01-05 14:30:29 +0100

    refactor: update password regex to allow only letters and digits
    Updated the password validation pattern to allow any combination of letters and digits while still enforcing at least one letter and one digit. This provides broader flexibility in password formats while maintaining security requirements.
    
    Signed-off-by: JavierDeSant <j.desantiagosoto@gmail.com>

--- src/main/java/com/javierdesant/spring_sport_flow/api/dto/requests/UserRequest.java
+++ src/main/java/com/javierdesant/spring_sport_flow/api/dto/requests/UserRequest.java
@@ -22,7 +22,7 @@ public abstract class UserRequest implements Serializable {
 
     @NotBlank
     @Size(min = 8)
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d).+$")
     private String password;
 
     private String passwordConfirmation;
